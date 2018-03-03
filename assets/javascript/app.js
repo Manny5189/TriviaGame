38fca016596dc08fc8ee4c83d98b46a95815ea81
Manuel De Jesus Martinez <manuelito@Manuels-MacBook-Pro.local>
2018-03-03 00:14:18 -0600

    First Update of the day

---
+++ assets/javascript/app.js
@@ -0,0 +1,46 @@
+var quizContainer = document.getElementById("quiz");
+var resultsContainer = document.getElementById("results");
+var submitButton = document.getElementById("submit")
+
+function buildQuiz(){
+
+
+}
+
+function showResults(){
+
+
+}
+
+buildQuiz();
+
+sumbit.button.addEventListener("click", showResults);
+
+var questions =[{
+Question1 : "Questions one?",
+answer1: {
+    a: "answerOne",
+    b: "answerTwo",
+    c: "answerThree",
+},
+    correctAnswer: "a"
+},
+{
+Question2 : "Question two?",
+answer2: {
+    a: "answeronea",
+    b:  "answertwoa",
+    c:  "answerthreea",
+},
+    correctAnswer: "b"
+},
+{
+    Question3 : "Question three?",
+    answer2: {
+        a: "answeroneb",
+        b:  "answertwob",
+        c:  "answerthreeb",
+    },
+        correctAnswer: "c"
+}
+];
